2d8a79a932da76a6ade6ef28d1e58acb667def41
Jorge Aguilar <anim85jorge@gmail.com>
2022-12-28 16:10:10 -0800

    building NavBar with MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { useMemo } from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Dashboard from "scene/dashboard";
+import Layout from "scene/layout"
+
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,9 @@
+const { styled } = require("@mui/system");
+
+const FlexBetween = styled(box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+});
+
+export default FlexBetween;
