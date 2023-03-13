056836f9676d01b5abb4799573b333e2c5cfcc80
Jorge Aguilar <anim85jorge@gmail.com>
2023-03-13 14:34:14 -0700

    Products Page Complete

--- client/src/scene/products/index.jsx
+++ client/src/scene/products/index.jsx
@@ -88,15 +88,14 @@ const Porducts = () => {
   const { data, isLoading } = useGetProductsQuery();
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
 
-  console.log("data", data);
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="Products" subtitle="See your list of products." />
+      <Header title="PRODUCTS" subtitle="See your list of products." />
       {data || !isLoading ? (
         <Box
           mt="20px"
           display="grid"
-          gridTemplateColumns="repeat (4, minmax(0, 1fr))"
+          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
           justifyContent="space-between"
           rowGap="20px"
           columnGap="1.33%"
@@ -124,10 +123,9 @@ const Porducts = () => {
                 rating={rating}
                 category={category}
                 supply={supply}
-                stat={sta}
+                stat={stat}
               />
-
-            )
+             )
           )}
         </Box>
       ) : (
