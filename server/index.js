e25c84ebac4b2a30c8a476ce9a8d400317dadde3
Jorge Aguilar <anim85jorge@gmail.com>
2023-04-03 15:30:56 -0700

    complete Transaction Page

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ import salesRoutes from './routes/sales.js';
 import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js"
+import Transaction from './models/Transaction.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
 
 /* CONFIGURATION */
 dotenv.config();
@@ -38,15 +39,19 @@ app.use("/sales", salesRoutes);
 const PORT  = process.env.PORT || 9000;
 /* possible error will come up in the future*/ 
 mongoose.set("strictQuery", false)
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+})
+.then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /*ONLY ADD DATA ONE TIME*/
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
+
 
 }).catch((error ) => console.log(`${error} did not connect`))
